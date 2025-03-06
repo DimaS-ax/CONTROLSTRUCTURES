5750a14136f1faaa5f940248adb1fea6bf21b79c
GamerPC <GamerPC@DESKTOP-K7RKTVQ>
2025-03-06 16:47:17 +0800

    PascalTriangle

--- PascalTriangle/PascalTriangle.cpp
+++ PascalTriangle/PascalTriangle.cpp
@@ -5,22 +5,25 @@ void main()
 {
 	setlocale(LC_ALL, "ru");
 
+	cout << "Pascal Triangle" << endl;
+
+
 	int n;
 	cout << "Введите количество строк: "; cin >> n;
 
 	for (int i = 0; i < n; i++)
-	{  // Выводим пробелы
+	{  
 		for (int c = 0; c < n - i - 1; c++)
 		{
 			cout << " ";
 		}
-		// Выводим элементы строки
+		
 		for (int j = 0; j <= i; j++)
 		{
 			int num = 1;
 			for (int k = 1; k <= j; k++)
-			{  // Формула для вычисления биномиальных коэффицентов
-				num = num * (i - k + 1) / k;
+			{  
+				num = num * (i - k + 1) / k; // Формула для вычисления биномиальных коэффицентов
 			}
 			cout << num << " ";
 		}
